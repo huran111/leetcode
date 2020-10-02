c4ed7068cf30573e40a05ff426f473f2b3a47339
huran <1345122890@qq.com>
2020-10-02 10:45:28 +0800

    jwt

--- jwt/src/main/java/jwt/JwtUtils.java
+++ jwt/src/main/java/jwt/JwtUtils.java
@@ -2,7 +2,6 @@ package jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
